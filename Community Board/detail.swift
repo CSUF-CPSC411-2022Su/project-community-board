81203ac5924ae7ca8c0f587067e06eef4e47c522
ahadhussain <36048343+ahadhussain@users.noreply.github.com>
2022-06-08 15:23:29 -0700

    Update detail.swift

--- Community Board/detail.swift
+++ Community Board/detail.swift
@@ -9,4 +9,11 @@ import Foundation
 
 struct detail {
     // add more code
+    
+    // Todo
+        // create a class that holds all of the details of a user. 
+    // Todo
+        // create an object of that class and store in a vector
+    // Todo
+        // sort vector by attributes and add filters
 }
