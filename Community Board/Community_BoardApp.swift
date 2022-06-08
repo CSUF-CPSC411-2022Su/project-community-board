bdc48309b11f1329d4886f3843b71c62534f3537
Nicky Valdivia <nicky.valdivia@csu.fullerton.edu>
2022-06-08 14:12:03 -0700

    Initial Commit

---
+++ Community Board/Community_BoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Community_BoardApp.swift
+//  Community Board
+//
+//  Created by csuftitan on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Community_BoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
