e483f7ad7490cdb790ee7b19cbab40bfadc3bfa7
Ahad Hussain <ahadhussain1999@csu.fullerton.edu>
2022-06-13 11:15:25 -0700

    Test Commit

--- Community Board/detail.swift
+++ Community Board/detail.swift
@@ -5,8 +5,18 @@
 //  Created by csuftitan on 6/8/22.
 //
 
-import Foundation
+import SwiftUI
 
-struct detail {
-    // add more code
+struct DetailView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
 }
+
+struct DetialView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
